0bece98201ad9d5982b24886755bd7df67a9a77e
fwilson <fwilson@fwilson.me>
2014-04-29 10:05:25 -0400

    fix unsafe port

--- server.js
+++ server.js
@@ -1,2 +1,3 @@
 var connect = require('connect');
-connect().use(connect.static(__dirname)).listen(process.env.PORT || 6000);
+connect().use(connect.static(__dirname)).listen(process.env.PORT || 5001);
+console.log("server started");
