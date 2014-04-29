2222afb9a4ce4187edde3cc8a07a3ce87ad1e150
fwilson <fwilson@fwilson.me>
2014-04-29 11:28:09 -0400

    fix paths

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 var connect = require('connect');
-connect().use(connect.static(__dirname)).listen(process.env.PORT || 5001);
+connect().use(connect.static(__dirname + '/html')).listen(process.env.PORT || 5001);
 console.log("server started");
