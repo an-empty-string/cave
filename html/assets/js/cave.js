5d7f67b919e2da60547e8f721494e99f64cad534
fwilson <fwilson@fwilson.me>
2014-04-29 16:41:39 -0400

    add rudimentary debugging

--- html/assets/js/cave.js
+++ html/assets/js/cave.js
@@ -1,18 +1,20 @@
 // cave
 // a text adventure engine, by Fox Wilson
 
-var settings = {}
+var settings = {
+    debug: true
+};
 
-var message = function(msg) {
-    $(".well").prepend(msg + "<br />");
-}
+var message = function(msg){$(".well").prepend(msg + "<br />");};
+var d = function(m){if(settings.debug)message("<i>debug: " + m + "</i>");};
+
 
 var welcome = function(game) {
     if(game.welcome) message(game.welcome);
     else {
         message("Welcome to " + game.title + ", by " + game.author);
     }
-}
+};
 
 $(document).ready(function() {
     message("cave initializing");
