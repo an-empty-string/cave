2cc814c47077ab1d6cbf5da5dc871990249cc154
fwilson <fwilson@fwilson.me>
2014-04-29 16:19:11 -0400

    on port n message

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 var connect = require('connect');
 connect().use(connect.static(__dirname + '/html')).listen(process.env.PORT || 5001);
-console.log("server started");
+console.log("server started on port " + (process.env.PORT || 5001));
