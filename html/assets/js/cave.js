cdb953c8a2dd1093412cfdb08f70aa07973b8112
fwilson <fwilson@fwilson.me>
2014-04-29 16:39:34 -0400

    welcome message

--- html/assets/js/cave.js
+++ html/assets/js/cave.js
@@ -7,10 +7,17 @@ var message = function(msg) {
     $(".well").prepend(msg + "<br />");
 }
 
+var welcome = function(game) {
+    if(game.welcome) message(game.welcome);
+    else {
+        message("Welcome to " + game.title + ", by " + game.author);
+    }
+}
+
 $(document).ready(function() {
     message("cave initializing");
     $.get("/game.json", function(data) {
-        console.log(data);
         message("cave initialization complete, entering game");
+        welcome(data);
     });
 });
